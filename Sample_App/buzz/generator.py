e91a6bb3199bc64524b05002791992910ca44db0
ctal80 <ctal80@gmail.com>
2018-12-22 22:35:32 +0200

    Update generator.py

--- Sample_App/buzz/generator.py
+++ Sample_App/buzz/generator.py
@@ -2,29 +2,31 @@
 from __future__ import print_function
 import random
 
-Buzz = ('continuous testing', 'continuous integration', 'continuous deployment',
+BUZZ = ('continuous testing', 'continuous integration', 'continuous deployment',
         'continuous improvement', 'devops')
 
-Adjectives = ('complete', 'modern', 'self-service',
+ADJECTIVES = ('complete', 'modern', 'self-service',
               'integrated', 'end-to-end')
 
-Adverbs = ('remarkably', 'enormously', 'substantially',
+ADVERBS = ('remarkably', 'enormously', 'substantially',
            'significantly', 'seriously')
 
-Verbs = ('accelerates', 'improves', 'enhances',
+VERBS = ('accelerates', 'improves', 'enhances',
          'revamps', 'boosts')
 
-'''Returns random words.'''
+
 def sample(listin, num=1):
+    '''Returns random words.'''
     result = random.sample(listin, num)
     if num == 1:
         return result[0]
     return result
 
-'''Return  buzzwords.'''
 def generate_buzz():
-    buzz_terms = sample(Buzz, 2)
-    phrase = ' '.join([sample(Adjectives), buzz_terms[0], sample(Adverbs), sample(Verbs), buzz_terms[1]])
+    '''Return  buzzwords.'''
+    buzz_terms = sample(BUZZ, 2)
+    phrase = ' '.join([sample(ADJECTIVES), buzz_terms[0], sample(ADVERBS),
+                       sample(VERBS), buzz_terms[1]])
     return phrase.title()
 
 if __name__ == "__main__":
